50f7422fae5588aeae701b2a89fdc6e987794fc7
Mark Rivera <mrivera7475@gmail.com>
2022-06-26 02:22:13 -0400

    syntax sugar code & comments created

---
+++ SyntaxSugar/Program.cs
@@ -0,0 +1,7 @@
+﻿//this is a syntax sugar code. Where the code is not required nor necessarily best practice, but is makes coding convient & concise.
+
+var answer = 4;
+
+var response = (answer > 0) ? $"{answer} is less than nine" : $"{answer} is greater than or equal to nine";
+
+Console.WriteLine(response);
